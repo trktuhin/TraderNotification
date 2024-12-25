f17f0f0afa91229ad70c94e0c096fa3f90c91d58
trktuhin <robin35-908@diu.edu.bd>
2024-12-25 11:49:05 +0600

    Complete working version

--- content.js
+++ content.js
@@ -2,17 +2,29 @@ let previousData = "";
 
 function checkForChanges() {
     logCurrentTime();
-    // const currentData = document.body.innerText; // Or a specific selector
-    // if (previousData && currentData !== previousData) {
-    //     new Audio(chrome.runtime.getURL("notify.mp3")).play();
-    // }
-    // previousData = currentData;
+
+    // read current order count
+    const targetEl = document.querySelector('div[data-auto="order_count"] .vBIeHEz6.hV4L34DK');
+    let orderCount = 'No orders';
+    if (targetEl) {
+        orderCount = targetEl.textContent.trim();
+    }
+
+    // read previous order count
+    const storedCount = localStorage.getItem('orderCount');
+
+    console.log("Previous:" + storedCount + " Latest:" + orderCount);
+
+    if (orderCount !== storedCount) {
+        chrome.runtime.sendMessage({ action: 'playNotification' });
+        localStorage.setItem('orderCount', orderCount);
+    }
 }
 
 function logCurrentTime() {
     const now = new Date();
     const formattedTime = now.toLocaleTimeString('en-US', { hour12: false });
-    console.log(`Current Time: ${formattedTime}`);
+    console.log(`Checking Time: ${formattedTime}`);
 }
 
 checkForChanges();
--- popup.js
+++ popup.js
@@ -1,6 +1,68 @@
+let intervalId = null;
+let selectedTabId;
+let phase = 1;
+
 const monitorSwitch = document.getElementById("monitorSwitch");
+
+chrome.runtime.onMessage.addListener((message) => {
+    if (message.action === 'playNotification') {
+        playNotification();
+    }
+});
 
 monitorSwitch.addEventListener("change", async () => {
     const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-    chrome.runtime.sendMessage({ action: monitorSwitch.checked ? "start" : "stop", tabId: tab.id });
+    if (monitorSwitch.checked) {
+        startMonitoring(tab.id);
+    } else {
+        stopMonitoring();
+    }
 });
+
+function playNotification() {
+    const soundUrl = chrome.runtime.getURL('sounds/notification.wav');
+    console.log("Sound url: ", soundUrl);
+    const audio = new Audio(soundUrl);
+    audio.play().then(() => {
+        console.log("Played")
+    }).catch((error) => {
+        console.error('Error playing the sound:', error);
+    });
+}
+
+function startMonitoring(tabId) {
+    if (!intervalId) {
+        intervalId = setInterval(() => {
+            console.log("Current phase:", phase);
+            logCurrentTime();
+            if (phase === 6) {
+                phase = 1;
+                chrome.tabs.reload(Number(tabId));
+                setTimeout(() => {
+                    chrome.scripting.executeScript({
+                        target: { tabId: Number(tabId) },
+                        files: ["content.js"]
+                    });
+                }, 5000);
+            }
+            else {
+                phase = phase + 1;
+            }
+
+        }, 10000); // 10 seconds
+    }
+}
+
+function stopMonitoring() {
+    selectedTabId = null;
+    if (intervalId) {
+        clearInterval(intervalId);
+        intervalId = null;
+    }
+}
+
+function logCurrentTime() {
+    const now = new Date();
+    const formattedTime = now.toLocaleTimeString('en-US', { hour12: false });
+    console.log(`Phase Time: ${formattedTime}`);
+}
